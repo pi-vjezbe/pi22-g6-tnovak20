eb0ca4502ad4d47efa9f9e71181c97314fd3836f
Programsko Inzenjerstvo <pi_foi@outlook.com>
2022-05-23 20:26:35 +0200

    Implementacija metoda klase Student.

--- Models/Student.cs
+++ Models/Student.cs
@@ -9,5 +9,101 @@ namespace Evaluation_Manager.Models
     public class Student : Person
     {
         public int Grade { get; set; }
+
+        public List<Evaluation> GetEvaluations()
+        {
+            return EvaluationRepository.GetEvaluation(this);
+        }
+
+        public int CalculateTotalPoints()
+        {
+            int totalPoints = 0;
+            foreach (var evaluation in GetEvaluations())
+            {
+                totalPoints += evaluation.Points;
+            }
+            return totalPoints;
+        }
+
+
+
+        public bool HasSignature()
+        {
+            bool hasSignature = true;
+            if (IsEvaluationComlete() == true)
+	        {
+                foreach (var evaluation in GetEvaluation())
+	            {
+                    if (evaluation.IsSufficientForSignature() == false)
+                    {
+                        hasSignature = false;
+                        break;
+                    }
+	            }
+	        }
+            else
+            {
+                hasSignature = false:
+            }
+	    }
+        return hasSignature;
+        
+        public bool HasGrade()
+        {
+            bool hasGrade = true;
+            if (IsEvaluationComlete() == true)
+	        {
+                foreach (var evaluation in GetEvaluation())
+	            {
+                    if (evaluation.IsSufficientForGrade() == false)
+                    {
+                        hasGrade = false;
+                        break;
+                    }
+	            }
+	        }
+            else
+            {
+                hasGrade = false:
+            }
+	    }
+        return hasGrade;
+
+        private bool IsEvaluationComlete()
+        {
+            var evaluations = GetEvaluations();
+            var activities = ActivityRepository.GetActivities();
+            return evaluations.Count == activities.Count;
+        }
+
+        public int CalculateGrade()
+        {
+            int grade = 0;
+            if (HasGrade() == true)
+            {
+                int totalPoints = CalculateTotalPoints();
+                if (totalPoints >= 91)
+                {
+                    grade = 5;
+                }
+                else if (totalPoints >= 76)
+                {
+                    grade = 4;
+                }
+                else if (totalPoints >= 61)
+                {
+                    grade = 3;
+                }
+                else if (totalPoints >= 50)
+                {
+                    grade = 2;
+                }
+                else
+                {
+                    grade = 1;
+                }
+            }
+            return grade;
+        }
     }
 }
